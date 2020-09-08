12298d24a3ee4e1b7e291e9fedc30bb644ba22e8
slilan <slilan96@gmail.com>
2020-09-08 06:12:26 +0300

    add parser; use gofmt for indentation

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -1,15 +1,50 @@
 package ast
 
+import "monkey/token"
+
 type Node interface {
-  TokenLiteral() string
+	TokenLiteral() string
 }
 
 type Statement interface {
-  Node
-  statementNode()
+	Node
+	statementNode()
 }
 
 type Expression interface {
-  Node
-  expressionNode()
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) statementNode() {}
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
 }
--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -4,63 +4,63 @@ type TokenType string // this allows us to define the tokens as constants or..en
 
 // define the type of the Token
 type Token struct {
-  Type TokenType
-  Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-  ILLEGAL = "ILLEGAL" // inform the parser that character is not identified
-  EOF     = "EOF"
+	ILLEGAL = "ILLEGAL" // inform the parser that character is not identified
+	EOF     = "EOF"
 
-  // Identifiers (or variables) + literals
-  IDENT = "IDENT"
-  INT   = "INT"
+	// Identifiers (or variables) + literals
+	IDENT = "IDENT"
+	INT   = "INT"
 
-  // OPERATORS
-  ASSIGN   = "="
-  PLUS     = "+"
-  MINUS    = "-"
-  BANG     = "!"
-  ASTERISK = "*"
-  SLASH    = "/"
-  LT       = "<"
-  GT       = ">"
-  EQ       = "=="
-  NOT_EQ   = "!="
+	// OPERATORS
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
 
-  // DELIMITERS 
-  COMMA     = ","
-  SEMICOLON = ";"
+	// DELIMITERS
+	COMMA     = ","
+	SEMICOLON = ";"
 
-  RPAREN    = ")"
-  LPAREN    = "("
-  RBRACE    = "}"
-  LBRACE    = "{"
+	RPAREN = ")"
+	LPAREN = "("
+	RBRACE = "}"
+	LBRACE = "{"
 
-  // KEYWORDS
-  FUNCTION = "FUNCTION"
-  LET      = "LET"
-  IF       = "IF"
-  ELSE     = "ELSE"
-  RETURN   = "RETURN"
-  TRUE     = "TRUE"
-  FALSE    = "FALSE"
+	// KEYWORDS
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
 )
 
-var keywords = map[string]TokenType {
-    "fn" :    FUNCTION,
-    "let":    LET,
-    "if" :    IF,
-    "else" :  ELSE,
-    "return": RETURN,
-    "true":   TRUE,
-    "false":  FALSE,
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
 }
 
 func LookupIdent(ident string) TokenType {
-    if tok, ok := keywords[ident]; ok {
-      return tok;
-    }
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
 
-    return IDENT
+	return IDENT
 }
